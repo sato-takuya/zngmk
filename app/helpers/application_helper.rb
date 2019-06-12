61a77d5754ea6740c71bf827d84063d5f5ea432b
SATOTAKUYA <[stakuya6@gmail.com]>
2019-06-12 18:51:13 +0900

    test

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,17 +5,19 @@ module ApplicationHelper
       if post.id.present?
         twitter_card[:url] = "https://zngmk.herokuapp.com/posts/#{post.public_uid}"
         twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/zangemaker-production/images/#{post.id}.png"
+        twitter_card[:description] = "#{post.content}"
       else
         twitter_card[:url] = 'https://zngmk.herokuapp.com/'
         twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/zangemaker-production/images/logo.png"
+        twitter_card[:description] = "#{post.content}"
       end
     else
       twitter_card[:url] = 'https://zngmk.herokuapp.com/'
       twitter_card[:image] = "https://s3-ap-northeast-1.amazonaws.com/zangemaker-production/images/logo.png"
+      twitter_card[:description] = "説明"
     end
     twitter_card[:title] = "懺悔メーカー"
     twitter_card[:card] = 'summary_large_image'
-    twitter_card[:description] = "#{post.content}"
     twitter_card
   end
 end
