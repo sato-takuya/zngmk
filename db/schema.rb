0761924a6b66cdc806b50a4c6ba448804cc8b52e
SATOTAKUYA <[stakuya6@gmail.com]>
2019-06-12 18:10:14 +0900

    DBに消してしまったindexを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_12_082454) do
+ActiveRecord::Schema.define(version: 2019_06_12_090508) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2019_06_12_082454) do
     t.string "image"
     t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["nickname"], name: "index_users_on_nickname", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
