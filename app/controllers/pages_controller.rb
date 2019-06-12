cb9b030b782c9cd0c1586bdfcb4d1c268a9d1514
SATOTAKUYA <[stakuya6@gmail.com]>
2019-06-12 16:10:08 +0900

    最低限のデザインOK

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
   def index
+    redirect_to page_url(current_user)
   end
 
   def show
+    #@posts = Post.where(user_id: current_user.id)
   end
 end
